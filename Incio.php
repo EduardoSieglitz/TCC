d2fc567edfb85f6d5317bd3ab2c1042c8335bc9f
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-04-20 23:51:07 -0300

    TCC

--- Incio.php
+++ Incio.php
@@ -3,11 +3,25 @@
     session_start();
     $_SESSION['email'];
 ?>
+    <head>
     <link rel="stylesheet" href="./CSS/styleInicio.css">
-    <div class="container">
-        <p><h2>Bem-vindo à Plataforma Bem Estar Total</h2>
-        <h2>Acompanhe sua Saúde de Forma Inteligente</h2>
-Na Plataforma Bem Estar Total, estamos comprometidos em ajudar você a cuidar do seu bem-estar físico e mental.
- Nossa abordagem integrada permite que você rastreie vários aspectos da sua saúde de maneira eficiente e eficaz.</p>
+    </head>
+    <div class="Container">
+        <div class="Left">
+            <div class="LeftTx">
+                <h1>Bem-vindo à Plataforma Bem Estar Total</h1>
+                <h2>Acompanhe sua Saúde de Forma Inteligente</h2>
+                <p>Na Plataforma Bem Estar Total, estamos comprometidos em ajudar você a cuidar do seu bem-estar físico e mental.
+                Nossa abordagem integrada permite que você rastreie vários aspectos da sua saúde de maneira eficiente e eficaz.</p>
+            </div>
+        </div>
+        <div class="Right">
+            <img src="./img/OIP.png" alt="">
+        </div>
+        <div class="Footer">
+           <h1>Bem estar total @2024</h1>
+        </div>
+        
+    </div>
        
        
