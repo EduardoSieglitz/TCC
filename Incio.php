c0be149055786c85aec65d952f0ccdd3fc476ca8
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-04-28 23:52:12 -0300

    Responsivo
    Estudo sobre site responsivo

--- Incio.php
+++ Incio.php
@@ -10,24 +10,14 @@
     </head>
     <div class="Container">
         <div class="Left">
-            <div class="LeftTx">
                 <h1>Bem-vindo à Plataforma Bem Estar Total</h1>
                 <h2>Acompanhe sua Saúde de Forma Inteligente</h2>
                 <p>Na Plataforma Bem Estar Total, estamos comprometidos em ajudar você a cuidar do seu bem-estar físico e mental.
                 Nossa abordagem integrada permite que você rastreie vários aspectos da sua saúde de maneira eficiente e eficaz.</p>
-            </div>
         </div>
         <div class="Right">
-        <div class="RightTx">
             <img src="./img/10-dicas-vida-saudavel-1-1024x683.png" alt="">
         </div>
-        </div>
-        <div class="Footer">
-        <div class="FooterTX">
-           <h1>Bem estar total para 2024</h1>
-        </div>
-        </div>
-        
     </div>
        
        
