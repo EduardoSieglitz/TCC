721e8c8a02cdc7bef58ce9a0f499726fc0811fc2
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-05-18 23:25:45 -0300

    Comentário
    Melhorando a responsividade da pagina.

--- Incio.php
+++ Incio.php
@@ -3,6 +3,7 @@
     session_start();
     if(isset($_SESSION['email'])){
         $_SESSION['email'];
+        $email = $_SESSION['email'];
     }
 ?>
     <head>
@@ -11,7 +12,7 @@
     <div class="Container">
         <div class="Left">
             <div class="Control">
-                <h1>Bem-vindo à Plataforma Bem Estar Total</h1>
+                <h1>Bem-vindo à Plataforma Bem Estar Total </h1>
                 <h2>Acompanhe sua Saúde de Forma Inteligente</h2>
                 <p>Na Plataforma Bem Estar Total, estamos comprometidos em ajudar você a cuidar do seu bem-estar físico e mental.
                 Nossa abordagem integrada permite que você rastreie vários aspectos da sua saúde de maneira eficiente e eficaz.</p>
