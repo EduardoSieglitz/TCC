9e3af32db4aad2b46847883a6239c6324e7d23e5
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-06-06 23:58:47 -0300

    Comentário
    melhores do a responsividade do site.

--- Script/scriptInicio.js
+++ Script/scriptInicio.js
@@ -1,6 +1,24 @@
 class navbar{
-    constructor(){
+    constructor(ControlIndex, abaRight){
+        this.ControlIndex = document.querySelector(ControlIndex);
+        this.abaRight = document.querySelector(abaRight);
+        this.Conteiner = "active";
+    }
+    addClickEvent(){
+        this.abaRight.addEventListener("click", () => console.log
+    ("dded"))
+    }
 
+    init(){
+        if(this.abaRight){
+            this.abaRight();
+        }
+        return this;
     }
 
 }
+const mobile = new navbar(
+    "abaRight",
+    "Aba",
+);
+mobile.init();
