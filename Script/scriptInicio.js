d9a926be96c5166a8151e8d345a9f1733a11b381
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-06-08 21:23:47 -0300

    Comentário
    Terminado a navbar responsiva.

--- Script/scriptInicio.js
+++ Script/scriptInicio.js
@@ -1,6 +1,13 @@
-function navbar(){
-        addEventListener()
-        const formcontrol = div.parentElement
-                formcontrol.className = 'aa'
-        console.log("dded");
+const aba = document.getElementById("abaRight");
+const abaMenu = document.getElementById("abaRightMenu");
+
+function clickMenu(){
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
 }
+function click(){
+    const navMenu = document.getElementById("nav");
+    navMenu.classList.toggle("active");
+}
+aba.addEventListener("click", clickMenu)
+abaMenu.addEventListener("click", clickMenu)
