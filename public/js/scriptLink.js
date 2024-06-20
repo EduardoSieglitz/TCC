4c80d28e55f86e25dbe9d952d28be2cb816b08fb
Eduardo Sieglitz da Silveira <133911631+EduardoSieglitz@users.noreply.github.com>
2024-06-20 09:50:12 -0300

    Comentário
    - Adicionado caminho dos botoes para as páginas.
    - Mudado pastas para a pasta raiz "public".
    - Alterado caminhos para funcionar com a pasta "public".
    - Arrumado erros de transições com CSS.

---
+++ public/js/scriptLink.js
@@ -0,0 +1,8 @@
+const Menu = document.getElementById("MenuRight");
+
+function click(){
+    const navMenu = document.getElementById("nav");
+    navMenu.classList.toggle("active");
+}
+Menu.addEventListener("click", click)
+
