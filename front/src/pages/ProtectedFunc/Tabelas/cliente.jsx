b74acca4b04f8408dbb0b7a60d0c8fd73b99521b
Eduardo Sieglitz da Silveira <133911631+EduardoSieglitz@users.noreply.github.com>
2024-09-25 21:38:28 -0300

    Criando Delete e Editar para a tabela funcionário
     Criando a parte de delete e editar para a tabelo do funcionário e adicionando Validação a tabela funcionário e cliente.

--- front/src/pages/ProtectedFunc/Tabelas/cliente.jsx
+++ front/src/pages/ProtectedFunc/Tabelas/cliente.jsx
@@ -1,62 +1,62 @@
 import React, { useEffect, useState } from 'react';
-import styles from './cliente.module.css';
-import axios from 'axios';
+import styles from './cliente.module.css'; 
+import Axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import validator from 'validator';
 
 const TabelaCliente = () => {
   const [users, setUsers] = useState([]);
   const [editUserId, setEditUserId] = useState(null);
-  const [editedUser, setEditedUser] = useState({
-    nome: '',
-    endereco: '',
-    cpf: '',
-    telefone: '',
-    email: '',
-    senha: ''
-  });
-  //Pegar Dados
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+  const [error, setError] = useState('');
+
   const fetchData = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/tabelacliente');
+      const response = await Axios.post('http://localhost:3001/tabelacliente');
       setUsers(response.data);
-      console.log(response.data)
     } catch (error) {
       console.error(error);
     }
   };
-  
-  //Deletar
+
   const handleDelete = async (idCliente) => {
     try {
-      await axios.delete(`http://localhost:3001/clientes/${idCliente}`);
-      fetchData(); 
+      await Axios.delete(`http://localhost:3001/clientes/${idCliente}`);
+      fetchData(); // Atualiza a lista após deletar
     } catch (error) {
       console.error('Erro ao deletar:', error);
     }
   };
-  //Tabela e Editar
+
   const handleEdit = (user) => {
     setEditUserId(user.idCliente);
-    setEditedUser(user);
+    // Preenche os valores no formulário para edição
+    setValue("nome", user.nome);
+    setValue("endereco", user.endereco);
+    setValue("cpf", user.cpf);
+    setValue("telefone", user.telefone);
+    setValue("email", user.email);
+    setValue("senha", user.senha);
   };
 
-  //Editar
-  const handleSave = async () => {
+  const handleSave = async (data) => {
     try {
-      await axios.put(`http://localhost:3001/clientes/${editUserId}`, editedUser);
-      setEditUserId(null);
-      fetchData();
+      const response = await Axios.put(`http://localhost:3001/clientes/${editUserId}`, data);
+      if (response.data === "Cadastrado") {
+        setError("");
+        setEditUserId(null); // Sai do modo de edição
+        fetchData(); // Atualiza a lista após salvar
+      } else if (response.data === "Email") {
+        setError("Email já existe");
+      } else if (response.data === "Telefone") {
+        setError("Telefone já existe");
+      } else if (response.data === "CPF") {
+        setError("CPF já existe");
+      }
     } catch (error) {
       console.error('Erro ao salvar:', error);
     }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditedUser({
-      ...editedUser,
-      [name]: value
-    });
   };
 
   useEffect(() => {
@@ -64,57 +64,103 @@ const TabelaCliente = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr>
-            <th>idCliente</th>
-            <th>Nome</th>
-            <th>Endereço</th>
-            <th>CPF</th>
-            <th>Telefone</th>
-            <th>Email</th>
-            <th>Senha</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.idCliente} className={styles.row}>
-              {editUserId === user.idCliente ? (
-                <>
-                  <td>{user.idCliente}</td>
-                  <td><input type="text" name="nome" value={editedUser.nome} onChange={handleChange} /></td>
-                  <td><input type="text" name="endereco" value={editedUser.endereco} onChange={handleChange} /></td>
-                  <td><input type="text" name="cpf" value={editedUser.cpf} onChange={handleChange} /></td>
-                  <td><input type="text" name="telefone" value={editedUser.telefone} onChange={handleChange} /></td>
-                  <td><input type="text" name="email" value={editedUser.email} onChange={handleChange} /></td>
-                  <td><input type="text" name="senha" value={editedUser.senha} onChange={handleChange} /></td>
-                  <td>
-                    <button onClick={handleSave}>Salvar</button>
-                    <button onClick={() => setEditUserId(null)}>Cancelar</button>
-                  </td>
-                </>
-              ) : (
-                <>
-                  <td>{user.idCliente}</td>
-                  <td>{user.nome}</td>
-                  <td>{user.endereco}</td>
-                  <td>{user.cpf}</td>
-                  <td>{user.telefone}</td>
-                  <td>{user.email}</td>
-                  <td>{user.senha}</td>
-                  <td>
-                    <button onClick={() => handleEdit(user)}>Editar</button>
-                    <button onClick={() => handleDelete(user.idCliente)}>Deletar</button>
-                  </td>
-                </>
-              )}
+    <div className={styles.body}>
+      <div className={styles.container}>
+        {error && <p className={styles.error_message}>{error}</p>}
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th>idCliente</th>
+              <th>Nome</th>
+              <th>Endereço</th>
+              <th>CPF</th>
+              <th>Telefone</th>
+              <th>Email</th>
+              <th>Senha</th>
+              <th>Ações</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.idCliente}>
+                {editUserId === user.idCliente ? (
+                  <>
+                    <td>{user.idCliente}</td>
+                    <td>
+                      <input type="text" placeholder="Nome"
+                        {...register('nome', { required: true, maxLength: 50, minLength: 2 })}
+                        className={errors?.nome && styles.input_error}
+                      />
+                      {errors?.nome && <p className={styles.input_menssage}>Nome inválido</p>}
+                    </td>
+                    <td>
+                      <input type="text" placeholder="Endereço"
+                        {...register("endereco", { required: true, maxLength: 100 })}
+                        className={errors?.endereco && styles.input_error}
+                      />
+                      {errors?.endereco && <p className={styles.input_menssage}>Endereço inválido</p>}
+                    </td>
+                    <td>
+                      <input type="text" placeholder="CPF"
+                        {...register('cpf', { required: true })}
+                        className={errors?.cpf && styles.input_error}
+                      />
+                      {errors?.cpf && <p className={styles.input_menssage}>CPF inválido</p>}
+                    </td>
+                    <td>
+                      <input type="text" placeholder="Telefone"
+                        {...register("telefone", {
+                          required: true,
+                          validate: (value) => /^[1-9]{2}-?[9]{0,1}[0-9]{4}-?[0-9]{4}$/.test(value)
+                        })}
+                        className={errors?.telefone && styles.input_error}
+                      />
+                      {errors?.telefone && <p className={styles.input_menssage}>Telefone inválido</p>}
+                    </td>
+                    <td>
+                      <input type="text" placeholder="Email"
+                        {...register('email', {
+                          required: true,
+                          minLength: 6,
+                          maxLength: 50,
+                          validate: (value) => validator.isEmail(value)
+                        })}
+                        className={errors?.email && styles.input_error}
+                      />
+                      {errors?.email && <p className={styles.input_menssage}>Email inválido</p>}
+                    </td>
+                    <td>
+                      <input type="password" placeholder="Senha"
+                        {...register("senha", { required: true, maxLength: 30, minLength: 8 })}
+                        className={errors?.senha && styles.input_error}
+                      />
+                      {errors?.senha && <p className={styles.input_menssage}>Senha inválida</p>}
+                    </td>
+                    <td>
+                      <button onClick={handleSubmit(handleSave)}>Salvar</button>
+                      <button onClick={() => setEditUserId(null)}>Cancelar</button>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>{user.idCliente}</td>
+                    <td>{user.nome}</td>
+                    <td>{user.endereco}</td>
+                    <td>{user.cpf}</td>
+                    <td>{user.telefone}</td>
+                    <td>{user.email}</td>
+                    <td>{user.senha}</td>
+                    <td>
+                      <button onClick={() => handleEdit(user)}>Editar</button>
+                      <button onClick={() => handleDelete(user.idCliente)}>Deletar</button>
+                    </td>
+                  </>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className={styles.line}></div>
         <Link to="/homefunc" className={styles.return}>Voltar</Link>
       </div>
     </div>
